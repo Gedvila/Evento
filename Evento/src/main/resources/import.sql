45d8fd053019714f2678b3dff4f878395fa13476
João Pedro Sani Gedvila <jpgedvila0306@gmail.com>
2025-03-26 16:07:11 -0300

    Finalização do projeto e seeding do banco de dados

--- Evento/src/main/resources/import.sql
+++ Evento/src/main/resources/import.sql
@@ -6,6 +6,15 @@ INSERT INTO tb_participantes(nome,email) VALUES ('Tiago Faria','tiago@gmail.com'
 INSERT INTO tb_participantes(nome,email) VALUES ('Maria do Rosário','maria@gmail.com');
 INSERT INTO tb_participantes(nome,email) VALUES ('Teresa Silva','teresa@gmail.com');
 
-INSERT INTO tb_blocos(inicio,fim) VALUES ('25/09/2017 08:00:00','25/09/2017 11:00:00');
-INSERT INTO tb_blocos(inicio,fim) VALUES ('25/09/2017 14:00:00','25/09/2017 18:00:00');
-INSERT INTO tb_blocos(inicio,fim) VALUES ('26/09/2017 08:00:00','26/09/2017 11:00:00');
+INSERT INTO  tb_atividades(categoria_id,nome,descricao,preco) VALUES (1,'Curso de HTML','Aprenda HTML de forma prática',80.00);
+INSERT INTO  tb_atividades(categoria_id,nome,descricao,preco) VALUES (2,'Oficina de Github','Controle versões de seus produtos',50.00);
+
+INSERT INTO tb_participante_atividade(atividade_id,participante_id) VALUES (1,1);
+INSERT INTO tb_participante_atividade(atividade_id,participante_id) VALUES (1,2);
+INSERT INTO tb_participante_atividade(atividade_id,participante_id) VALUES (1,3);
+INSERT INTO tb_participante_atividade(atividade_id,participante_id) VALUES (2,3);
+INSERT INTO tb_participante_atividade(atividade_id,participante_id) VALUES (2,4);
+
+INSERT INTO tb_blocos(atividade_id,inicio,fim) VALUES (1,'2017-09-25T08:00:00','2017-09-25T11:00:00');
+INSERT INTO tb_blocos(atividade_id,inicio,fim) VALUES (2,'2017-09-25T14:00:00','2017-09-25T18:00:00');
+INSERT INTO tb_blocos(atividade_id,inicio,fim) VALUES (2,'2017-09-26T08:00:00','2017-09-26T11:00:00');
